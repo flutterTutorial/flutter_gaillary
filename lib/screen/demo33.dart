966e818779bc71d94fa17de50acdedb2a4e293bd
ashokverma1 <ashok@cyberframe.in>
2024-01-05 22:49:14 +0530

    textfiled

--- lib/screen/demo33.dart
+++ lib/screen/demo33.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gallery/provider/testprovider.dart';
+import 'package:flutter_gallery/utils/mycolors.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 
-class YourOtherPage extends StatelessWidget {
+class YourOtherPage extends StatefulWidget {
+  @override
+  State<YourOtherPage> createState() => _YourOtherPageState();
+}
+
+class _YourOtherPageState extends State<YourOtherPage> {
+  TextEditingController _mobileController = new TextEditingController();
+  bool _suffixIcon = false, _isLoading = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,6 +22,50 @@ class YourOtherPage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            TextField(
+          textAlignVertical: TextAlignVertical.center,
+          controller: _mobileController,
+          cursorColor: MyColors.toolDark,
+          style: GoogleFonts.roboto(
+            color: MyColors.textcolor,
+            fontWeight: FontWeight.w600,
+            fontSize: 16.0,
+          ),
+          onChanged: (value) {
+            setState(() {
+              if (value.length == 10) {
+                _suffixIcon = true;
+              } else {
+                _suffixIcon = false;
+              }
+            });
+          },
+          maxLength: 10,
+          keyboardType: TextInputType.number,
+          decoration: InputDecoration(
+            counterText: '',
+            fillColor: MyColors.textfiledbg,
+            border: InputBorder.none,
+            errorBorder: InputBorder.none,
+            enabledBorder: InputBorder.none,
+            disabledBorder: InputBorder.none,
+            focusedBorder: InputBorder.none,
+            focusedErrorBorder: InputBorder.none,
+            icon: Icon(
+              Icons.smartphone_rounded,
+              color: MyColors.textcolor,
+            ),
+            suffixIcon: _suffixIcon
+                ? Icon(
+                    Icons.check_circle_rounded,
+                    color: MyColors.textcolor,
+                  )
+                : Icon(
+                    Icons.check_circle_outline_rounded,
+                    color: MyColors.textcolor,
+                  ),
+          ),
+        ),
             Consumer<OTPProvider>(
               builder: (context, otpProvider, child) {
                 return Text('OTP Status: ${otpProvider.otpStatus}');
---
+++ lib/utils/mycolors.dart
@@ -0,0 +1,44 @@
+import 'dart:ui';
+
+class MyColors {
+  static const Color black = const Color(0xff000000);
+  static const Color white = const Color(0xffffffff);
+  static const Color statusDark = const Color(0xFF29417E);
+  static const Color toolDark = const Color(0xFF29417E);
+  static const Color gradientDark = const Color(0xFF29417E);
+  static const Color gradientLight = const Color(0xBE29417E);
+  static const Color toollight = const Color(0xFF627FC8);
+  static const Color toolmedium = const Color(0xFF334E91);
+  static const Color buttoncolor = const Color(0xFFE2B378);
+  static const Color buttonlightcolor = const Color(0xFFEBCE83);
+  static const Color creditcolor = const Color(0xFFC5FFD3);
+  static const Color debitcolor = const Color(0xFFFFE1E1);
+
+  static const Color textcolor = const Color(0xFF808080);
+  static const Color textfiledbg = const Color(0xFFF3F3F3);
+  static const Color otpbg = const Color(0xFFDFDFDF);
+  static const Color appbg = const Color(0xFFF8F8FA);
+  static const Color homebg = const Color(0xFFEFEFEF);
+
+  static const Color grey = const Color(0xFFCBD1D2);
+
+  static const Color btncolor = const Color(0xffD03169);
+  static const Color btnfirst = const Color(0xff5fb7c5);
+  static const Color titlebg = const Color(0xfff3f9fe);
+  static const Color datecolor = const Color(0xffFAF3F8);
+  static const Color recinnertitle = const Color(0xffDDE7EC);
+  static const Color recordtitle = const Color(0xffccdde5);
+  static const Color white_transparent = const Color(0xB3FFFFFF);
+  static const Color black_transparent = const Color(0x4c000000);
+  static const Color medium_black_transparent = const Color(0xFF555555);
+  static const Color medium_grey_transparent = const Color(0xFFEFEFEF);
+
+  static const Color dashboardgreen = const Color(0xff42d5a9);
+  static const Color dashboardv2v = const Color(0xffffaf44);
+  static const Color dashboardpur = const Color(0xffb98ff3);
+  static const Color dashboardorn = const Color(0xfff9a299);
+  static const Color dashboardpn = const Color(0xfff88ddd);
+  static const Color dashboardbl = const Color(0xff95d5f8);
+  static const Color dashboardbrown = const Color(0xffcd8282);
+  static const Color dashfusion = const Color(0xffAEAB79);
+}
