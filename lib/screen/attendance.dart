40c9405db23e45ec83a256a6af97860f1e6c79da
ashokverma1 <ashok@cyberframe.in>
2024-01-30 22:26:19 +0530

    calen2

--- lib/screen/attendance.dart
+++ lib/screen/attendance.dart
@@ -77,33 +77,33 @@ class _DemoTestState extends State<DemoTest> {
                         lastDay: DateTime(2100),
                         focusedDay: _focusedDay,
                         calendarFormat: _calendarFormat,
-                        selectedDayPredicate: (day) {
-                          // Use `selectedDayPredicate` to determine which day is currently selected.
-                          // If this returns true, then `day` will be marked as selected.
+                        // selectedDayPredicate: (day) {
+                        //   // Use `selectedDayPredicate` to determine which day is currently selected.
+                        //   // If this returns true, then `day` will be marked as selected.
 
-                          // Using `isSameDay` is recommended to disregard
-                          // the time-part of compared DateTime objects.
-                          return isSameDay(_selectedDay, day);
-                        },
+                        //   // Using `isSameDay` is recommended to disregard
+                        //   // the time-part of compared DateTime objects.
+                        //   return isSameDay(_selectedDay, day);
+                        // },
                          headerStyle: const HeaderStyle(
                             formatButtonVisible: false, titleCentered: true),
-                        onDaySelected: (selectedDay, focusedDay) {
-                          if (!isSameDay(_selectedDay, selectedDay)) {
-                            // Call `setState()` when updating the selected day
-                            setState(() {
-                              _selectedDay = selectedDay;
-                              _focusedDay = focusedDay;
-                            });
-                          }
-                        },
-                        onFormatChanged: (format) {
-                          if (_calendarFormat != format) {
-                            // Call `setState()` when updating calendar format
-                            setState(() {
-                              _calendarFormat = format;
-                            });
-                          }
-                        },
+                        // onDaySelected: (selectedDay, focusedDay) {
+                        //   if (!isSameDay(_selectedDay, selectedDay)) {
+                        //     // Call `setState()` when updating the selected day
+                        //     setState(() {
+                        //       _selectedDay = selectedDay;
+                        //       _focusedDay = focusedDay;
+                        //     });
+                        //   }
+                        // },
+                        // onFormatChanged: (format) {
+                        //   if (_calendarFormat != format) {
+                        //     // Call `setState()` when updating calendar format
+                        //     setState(() {
+                        //       _calendarFormat = format;
+                        //     });
+                        //   }
+                        // },
                         onPageChanged: (focusedDay) {
                           // No need to call `setState()` here
                           _focusedDay = focusedDay;
